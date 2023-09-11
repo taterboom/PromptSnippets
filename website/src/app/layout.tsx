80396248131e1027c0fd8ee6831586fef2d84661
xueyong <1085523029@qq.com>
2023-09-12 00:28:46 +0800

    📈 add GA

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -13,6 +13,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RCWDT5TD5S"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RCWDT5TD5S');`,
+          }}
+        ></script>
+      </head>
       <body className={inter.className}>{children}</body>
       <Script src="/playground-cc70ed46.js"></Script>
     </html>
