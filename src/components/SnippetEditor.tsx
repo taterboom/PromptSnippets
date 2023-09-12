204c008f2af1d87ea3f48f2314bbb47f2e4bd410
xueyong <1085523029@qq.com>
2023-09-12 18:45:01 +0800

    ✨ add Tooltip

--- src/components/SnippetEditor.tsx
+++ src/components/SnippetEditor.tsx
@@ -3,6 +3,8 @@ import { motion } from "framer-motion"
 import { useSnippets } from "../store/snippets"
 import { PopupContainer } from "./UI/Popup"
 import { usePageState } from "../store/pageState"
+import { Tooltip, TooltipContent, TooltipTrigger } from "./UI/Tooltip"
+import { MiCircleHelp } from "./UI/icons"
 
 type SnippetEditorProps = {
   id?: string
@@ -22,10 +24,17 @@ export default function SnippetEditor(props: SnippetEditorProps) {
   return (
     <PopupContainer onClick={props.onClose}>
       <div className="space-y-1">
-        <div className="text-sm text-content-100">Prefix</div>
-        <div className="text-xs text-content-400">{`The prefix to be used when selecting the snippet in the snippets popup after typing "${
-          triggerSymbol[0] ?? ""
-        }prefix".`}</div>
+        <div className="text-sm text-content-100 flex items-center gap-1">
+          <span>Prefix</span>
+          <Tooltip placement="top-start">
+            <TooltipTrigger className="p-0.5">
+              <MiCircleHelp className="text-xs text-content-400" />
+            </TooltipTrigger>
+            <TooltipContent className="text-xs text-content-200 w-60 z-[1000000] bg-base-100/10 backdrop-blur border border-neutral-200 p-2 rounded menu-popup-shadow">{`The prefix to be used when selecting the snippet in the snippets popup after typing "${
+              triggerSymbol[0] ?? ""
+            }prefix".`}</TooltipContent>
+          </Tooltip>
+        </div>
         <input
           placeholder="e.g. translate"
           type="text"
@@ -37,8 +46,17 @@ export default function SnippetEditor(props: SnippetEditorProps) {
         />
       </div>
       <div className="space-y-1">
-        <div className="text-sm text-content-100">Content</div>
-        <div className="text-xs text-content-400">{`The snippet content. wrap variable values with ${wrapper[0]}VariableName${wrapper[1]}`}</div>
+        <div className="text-sm text-content-100 flex items-center gap-1">
+          <span>Content</span>
+          <Tooltip placement="top-start">
+            <TooltipTrigger className="p-0.5">
+              <MiCircleHelp className="text-xs text-content-400" />
+            </TooltipTrigger>
+            <TooltipContent className="text-xs text-content-200 w-60 z-[1000000] bg-base-100/10 backdrop-blur border border-neutral-200 p-2 rounded menu-popup-shadow">
+              {`The snippet content. wrap variable values with ${wrapper[0]}VariableName${wrapper[1]}`}
+            </TooltipContent>
+          </Tooltip>
+        </div>
         <textarea
           className="text-sm !mt-1.5 block bg-base-200 text-content-200 border border-neutral-200 rounded w-full py-1.5 px-2 focus:border-primary-100 focus-visible:outline-none"
           placeholder={`e.g. Translate the text to ${wrapper[0]}language${wrapper[1]}: ${wrapper[0]}text${wrapper[1]}`}
